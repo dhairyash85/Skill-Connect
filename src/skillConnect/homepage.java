f4ff34900ca70acda1e7d99850a13149a8cdb627
dhairyash85 <118038130+dhairyash85@users.noreply.github.com>
2023-09-08 23:14:58 +0530

    Update homepage.java

--- src/skillConnect/homepage.java
+++ src/skillConnect/homepage.java
@@ -1,7 +1,6 @@
 package skillConnect;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,6 +12,11 @@ import java.awt.TextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import skillConnect.login;
+import java.awt.Window.Type;
 
 public class homepage extends JFrame {
 
@@ -38,10 +42,12 @@ public class homepage extends JFrame {
 	 * Create the frame.
 	 */
 	public homepage() {
+		setResizable(false);
+		setType(Type.POPUP);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(200, 100, 900, 500);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -72,15 +78,43 @@ public class homepage extends JFrame {
 		JPanel Favourites = new JPanel();
 		Favourites.setBackground(new Color(204, 215, 215));
 		tabbedPane.addTab("Favourites", null, Favourites, null);
+		Favourites.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Ass and tits");
+		JLabel lblNewLabel = new JLabel("Favourites will be displayed here");
+		lblNewLabel.setBounds(10, 11, 207, 14);
 		Favourites.add(lblNewLabel);
 		
 		Panel account = new Panel();
 		account.setBackground(new Color(204, 215, 215));
 		tabbedPane.addTab("My Account", null, account, null);
+		account.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("Username: nigger");
+		JLabel lblNewLabel_1 = new JLabel("Username: admin");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(10, 86, 307, 48);
 		account.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("College: V.E.S.I.T");
+		lblNewLabel_2.setBounds(10, 130, 398, 14);
+		account.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Course: Engineering");
+		lblNewLabel_3.setBounds(10, 155, 398, 14);
+		account.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Year: 2nd Year");
+		lblNewLabel_4.setBounds(10, 180, 398, 14);
+		account.add(lblNewLabel_4);
+		
+		JButton logout = new JButton("Logout");
+		logout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				login n=new login();
+				n.setVisible(true);
+				dispose();
+			}
+		});
+		logout.setBounds(10, 393, 89, 23);
+		account.add(logout);
 	}
 }
